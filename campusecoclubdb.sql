e10318508af552e216f2b2cd3f4fc3506bad07be
Jevaanraj <jevaanraj17@gmail.com>
2026-02-02 00:27:55 +0800

    Add primary keys to multiple tables in SQL schema

--- campusecoclubdb.sql
+++ campusecoclubdb.sql
@@ -6,7 +6,8 @@ CREATE TABLE Users (
     Email varchar(200),
     UserPassword varchar(20),
     UserRole varchar(20),
-    UserStatus varchar(20)
+    UserStatus varchar(20),
+    PRIMARY KEY (UserID)
 );
 
 CREATE TABLE Student (
@@ -19,7 +20,8 @@ CREATE TABLE Event (
     EventDate date,
 	Location varchar(100),
     EventDescription varchar(150),
-	EventStatus varchar(50)
+	EventStatus varchar(50),
+    PRIMARY KEY (EventID)
 );
 
 CREATE TABLE RecyclingLog (
@@ -27,14 +29,16 @@ CREATE TABLE RecyclingLog (
 	LogType varchar(50),
     weight decimal(3,2),
     LogStatus varchar(20),
+    PRIMARY KEY (LogID),
     FOREIGN KEY (UserID) REFERENCES Users(UserID)
 );
 
 CREATE TABLE Task (
 	TaskID varchar(20),
     TaskName varchar (50),
-    TaskDescription (150),
-    TaskStatus (20),
+    TaskDescription varchar(150),
+    TaskStatus varchar(20),
+    PRIMARY KEY (TaskID),
     FOREIGN KEY (EventID) REFERENCES Events(EventID),
     FOREIGN KEY (UserID) REFERENCES Users(UserID)
 );
@@ -43,12 +47,14 @@ CREATE TABLE Notification (
 	NotificationID varchar(20),
     message varchar(30),
     NotificationTime datetime,
+    PRIMARY KEY (NotificationID),
     FOREIGN KEY (UserID) REFERENCES Users(UserID)
 );
 
 CREATE TABLE Registration (
 	RegistrationID varchar(20),
     RegistrationStatus varchar (20),
+    PRIMARY KEY (RegistrationID),
     FOREIGN KEY (EventID) REFERENCES Events(EventID),
     FOREIGN KEY (UserID) REFERENCES Users(UserID)
 );
@@ -57,5 +63,6 @@ CREATE TABLE Announcement (
 	AnnouncementID varchar(20),
     message varchar(30),
     AnnouncementDate datetime,
-     FOREIGN KEY (EventID) REFERENCES Events(EventID)
+    PRIMARY KEY (AnnouncementID),
+	FOREIGN KEY (EventID) REFERENCES Events(EventID)
 );
